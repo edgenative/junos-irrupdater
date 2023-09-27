5e4fd58ed328665899357c5c360a3a00b5db60cb
Lee Hetherington <70320109+edgenative@users.noreply.github.com>
2023-09-26 19:05:33 -0700

    Create junos-filtergen.py

---
+++ bin/junos-filtergen.py
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Script for generating BGP filter for Mikrotik RouterOS
+# (c) 2023 Lee Hetherington <lee@edgenative.net>
+
+path=/usr/share/junos-irrupdater
+
+bgpq4 -F '%n/%l \n' -4 -A $2 > $path/db/$1.4.agg
+bgpq4 -F '%n/%l \n' -6 -A $2 > $path/db/$1.6.agg
+root@flow:/usr/share/junos-irrupdater/bin# ls
+fetchprefixes.sh  junos-filtergen.py  junos-irrupdater.py
+root@flow:/usr/share/junos-irrupdater/bin# cat junos-filtergen.py
+#!/usr/bin/env python3
+# Script for generating BGP filters for Juniper JunOS
+# (c) 2023 Lee Hetherington <lee@edgenative.net>
+#
+
+import os
+import sys
+
+# Set the path configuration variable here
+path = "/usr/share/junos-irrupdater"
+
+def generate_ipv4_filter(asn):
+    with open(f"{path}/filters/as{asn}-import-ipv4.txt", "w") as f:
+        f.write("policy-options {\n")
+        f.write(f"policy-statement as{asn}-import-ipv4 {{\n")
+        f.write("apply-flags omit;\n")
+        f.write("	term prefixes {\n")
+        f.write("		from {\n")
+        with open(f"{path}/db/{asn}.4.agg", "r") as prefixes:
+            for prefix in prefixes:
+                prefix = prefix.strip()
+                masklength = int(prefix.split("/")[1])
+                if masklength == 24:
+                    f.write(f"			route-filter {prefix} exact;\n")
+                elif masklength < 24:
+                    f.write(f"			route-filter {prefix} upto /24;\n")
+        f.write("		}\n")
+        f.write("		then next term;\n")
+        f.write("	}\n")
+        f.write("	term reject {\n")
+        f.write("		then reject;\n")
+        f.write("	}\n")
+        f.write("}\n")
+        f.write("}\n")
+
+def generate_ipv6_filter(asn):
+    with open(f"{path}/filters/as{asn}-import-ipv6.txt", "w") as f:
+        f.write("policy-options {\n")
+        f.write(f"policy-statement as{asn}-import-ipv6 {{\n")
+        f.write("apply-flags omit;\n")
+        f.write("  term prefixes {\n")
+        f.write("		from {\n")
+        with open(f"{path}/db/{asn}.6.agg", "r") as prefixes6:
+            for prefix6 in prefixes6:
+                prefix6 = prefix6.strip()
+                masklength6 = int(prefix6.split("/")[1])
+                if masklength6 == 48:
+                    f.write(f"			route-filter {prefix6} exact;\n")
+                elif masklength6 < 48:
+                    f.write(f"			route-filter {prefix6} upto /48;\n")
+        f.write("		}\n")
+        f.write("		then next term;\n")
+        f.write("  }\n")
+        f.write("  term reject {\n")
+        f.write("          then reject;\n")
+        f.write("  }\n")
+        f.write("}\n")
+        f.write("}\n")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 junos-filtergen.py <ASN>")
+        sys.exit(1)
+
+    asn = sys.argv[1]
+
+    generate_ipv4_filter(asn)
+    generate_ipv6_filter(asn)
