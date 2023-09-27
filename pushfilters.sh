abd44652064446743ebf1d7cf1bcf585528f726e
Lee Hetherington <70320109+edgenative@users.noreply.github.com>
2023-09-26 19:00:23 -0700

    Create pushfilters.sh

---
+++ pushfilters.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Script for generating BGP filter for Mikrotik RouterOS
+# (c) 2023 Lee Hetherington <lee@edgenative.net>
+
+path=/usr/share/junos-irrupdater
+
+# Check if the configuration file exists
+if [ ! -f $path/config/sessions.conf ]; then
+    echo "Configuration File 'sessions.conf' not found."
+    exit 1
+fi
+
+# Read the input file line by line
+while IFS=',' read -r param1 param2; do
+    if [ -n "$param1" ] && [ -n "$param2" ]; then
+        # Run bgpq4 to fetch the prefixes, with ASN $param1 and AS-SET $param2 as arguments
+	python3 $path/bin/junos-irrupdater.py $param2 as$param1-import-ipv4
+	python3 $path/bin/junos-irrupdater.py $param2 as$param1-import-ipv6
+    fi
+done < $path/config/sessions.conf
